0d1dd84141da11c9731680987bc6d1c180a2c274
er-ishita <ishita.arora1919@gmail.com>
2026-03-07 16:32:03 +0530

    Image overlay

--- main.py
+++ main.py
@@ -21,11 +21,56 @@ start=cv2.imread("Picture-Puzzle/start.png")
 detect=htm.HandDetector()
 clickPic=False
 snapped=None
+saved=snapped
+
+puz=None
+
+puzHeight,puzWidth=640,527
+vl1,vl2=int(puzHeight/3),int(2*puzHeight/3)
+hl1,hl2=int(puzWidth/3),int(2*puzWidth/3)
 
 def overlay(background, overlay):
-    alpha=0.6
+    alpha=0.2
     blend=cv2.addWeighted(background, 1-alpha, overlay,alpha,0)
+
+    h,w,_=overlay.shape
+    cv2.line(blend, (0,vl1), (w-1,vl1), (0,0,0), 2)
+    cv2.line(blend, (0,vl2), (w-1,vl2), (0,0,0), 2)
+
+    cv2.line(blend, (hl1,0), (hl1,h-1), (0,0,0), 2)
+    cv2.line(blend, (hl2,0), (hl2,h-1), (0,0,0), 2)
+
     return blend
+
+########################################under construction
+def puzzleRandom(snap):
+    a11=snap[0:hl1,0:vl1]
+    a12=snap[0:hl1,vl1:vl2]
+    a13=snap[0:hl1,vl2:puzWidth]
+
+    a21=snap[hl1:hl2,0:vl1]
+    a22=snap[hl1:hl2,vl1:vl2]
+    a23=snap[hl1:hl2,vl2:puzWidth]
+
+    a31=snap[hl2:puzHeight,0:vl1]
+    a32=snap[hl2:puzHeight,vl1:vl2]
+    a33=snap[hl2:puzHeight,vl2:puzWidth]
+
+    puzzle=np.array([
+        a11,a12,a13,
+        a21,a22,a23,
+        a31,a32,a33
+    ])
+
+    np.random.shuffle(puzzle)
+
+    puzzle=puzzle.reshape(3,3)
+    return puzzle
+
+def puzzleSwap(c1,c2,puzzle):
+    puzzle[c1],puzzle[c2]= puzzle[c2],puzzle[c1]
+
+###############################################################
 
 while True:
     succ, img=cam.read()
@@ -64,20 +109,20 @@ while True:
             lowx,lowy=min(tx1,tx2),min(ty1,ty2)
             highx,highy=max(tx1,tx2),max(ty1,ty2)
             snapped=img[lowy:highy, lowx:highx].copy()
-            lastSnap=time.time()
+            saved=snapped
+            # puz=puzzleRandom(snapped)
     
-    # if clickPic:
-    #     img=overlay(snapped, img)
+    if clickPic and snapped is not None:
+        h,w,_=img.shape
+        snapped=cv2.resize(snapped,(640,527))
+        img=overlay(snapped, img)
+
     if clickPic and snapped is not None:
         cv2.imshow("Snapp",snapped)
     
     frame=start.copy()
     frame[120:120+527,72:72+640]=img
-    
-    #ctime=time.time()
-    #fps=int(1/(ctime-ptime))
-    #ptime=ctime
-    #cv2.putText(img, f'FPS: {fps}', (10,70),cv2.FONT_HERSHEY_COMPLEX,3,(0,0,255),2)
+
     cv2.imshow("Picture-Puzzle",frame)
     
     key=cv2.waitKey(1)
